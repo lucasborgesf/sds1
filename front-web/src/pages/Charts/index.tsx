282d6619a10ee7f24f304e7b593827e26aadeef2
Lucas Borges <lucasbf.contato@gmail.com>
2020-09-20 16:34:19 -0300

    Configurando front-web para produção

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -21,7 +21,7 @@ const initialPieData = {
     series: []
 }
 
-const BASE_URL ='http://localhost:8080'
+const BASE_URL ='https://sds1-lb.herokuapp.com';
 
 const Charts = () => {
     const [BarChartData, setBarChartData] = useState<BarChartData[]>([]);
--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -6,7 +6,7 @@ import { formatDate } from './helpers';
 import Pagination from './Pagination';
 import Filter from '../../components/Filters';
 
-const BASE_URL ='http://localhost:8080'
+const BASE_URL ='https://sds1-lb.herokuapp.com'
 const Records = () => {
     const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
     const [activePage, setActivePage] = useState(0);
